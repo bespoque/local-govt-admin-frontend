fba6f9a2ff888ead02b1a02d832e4947d3cb394d
prinxard <prinxard@gmail.com>
2024-05-15 08:47:51 +0100

    update individual

--- src/components/modals/update-individual-modal.tsx
+++ src/components/modals/update-individual-modal.tsx
@@ -28,124 +28,234 @@ const UpdateIndividualTp: React.FC<Props> = ({
         <form onSubmit={handleUpdateSubmit}>
           <div className="grid grid-cols-3 gap-4 overflow-y-auto h-96 p-2" style={{ scrollbarWidth: 'thin' }}>
             <div>
-              <label htmlFor="companyname" className="block text-sm font-medium text-gray-700">company name:</label>
-              <input
-                type="text"
-                id="companyname"
-                name="companyname"
-                defaultValue={singleTpayer?.companyname || ''}
+              <label htmlFor="title" className="block text-sm font-medium text-gray-700">Title:</label>
+              <select
+                id="title"
+                name="title"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm  border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 bg-gray-100"
-              />
+                defaultValue={singleTpayer?.title || ''}
+                >
+                <option value="">Please select</option>
+                <option value="mrs">Mrs</option>
+                <option value="mr">Mr</option>
+                <option value="mss">Miss</option>
+              </select>
             </div>
             <div>
-              <label htmlFor="registeredname" className="block text-sm font-medium text-gray-700">registered name:</label>
+              <label htmlFor="firstname" className="block text-sm font-medium text-gray-700">Firstname:</label>
               <input
-                id="registeredname"
+                id="firstname"
                 type="text"
-                name="registeredname"
+                name="firstname"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm  border-cyan-800 border bg-gray-100  rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.registeredname || ''}
-              />
+                defaultValue={singleTpayer?.firstname || ''}
+                />
             </div>
             <div>
-              <label htmlFor="businesstype" className="block text-sm font-medium text-gray-700">business type:</label>
+              <label htmlFor="middlename" className="block text-sm font-medium text-gray-700">Middlename:</label>
               <input
-                id="businesstype"
+                id="middlename"
                 type="text"
-                name="businesstype"
+                name="middlename"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.businesstype || ''}
-              />
+                defaultValue={singleTpayer?.middlename || ''}
+                />
             </div>
             <div>
-              <label htmlFor="rc" className="block text-sm font-medium text-gray-700">rc:</label>
+              <label htmlFor="surname" className="block text-sm font-medium text-gray-700">Surname:</label>
               <input
-                id="rc"
+                id="surname"
                 type="text"
-                name="rc"
+                name="surname"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block border w-full shadow-sm sm:text-sm  border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.rc || ''}
-              />
+                defaultValue={singleTpayer?.surname || ''}
+                />
             </div>
             <div>
-              <label htmlFor="regno" className="block text-sm font-medium text-gray-700">Reg no:</label>
+              <label htmlFor="dob" className="block text-sm font-medium text-gray-700">Date of Birth:</label>
               <input
-                id="regno"
-                type="text"
-                name="regno"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.regno || ''}
-              />
-            </div>
-            <div>
-              <label htmlFor="companytin" className="block text-sm font-medium text-gray-700">company Tin:</label>
-              <input
-                id="companytin"
-                type="text"
-                name="companytin"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer.companytin || ''}
-              />
-            </div>
-            <div>
-              <label htmlFor="datecommenced" className="block text-sm font-medium text-gray-700">date commenced:</label>
-              <input
+                id="dob"
                 type="date"
-                id="datecommenced"
-                name="datecommenced"
+                name="dob"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.datecommenced || ''}
-              />
+                defaultValue={singleTpayer?.dob || ''}
+                />
             </div>
             <div>
-              <label htmlFor="dateincorporated" className="block text-sm font-medium text-gray-700">date incorporated:</label>
-              <input
-                type="date"
-                id="dateincorporated"
-                name="dateincorporated"
+              <label htmlFor="gender" className="block text-sm font-medium text-gray-700">Gender:</label>
+              <select
+                id="gender"
+                name="gender"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer.dateincorporated || ''}
-              />
+                defaultValue={singleTpayer?.gender || ''}
+                >
+                <option value="">Select</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
             </div>
             <div>
-              <label htmlFor="sector" className="block text-sm font-medium text-gray-700">sector:</label>
-              <input
-                id="sector"
-                type="text"
-                name="sector"
+              <label htmlFor="maritalstatus" className="block text-sm font-medium text-gray-700">Marital status:</label>
+              <select
+                id="maritalstatus"
+                name="maritalstatus"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 "
-                defaultValue={singleTpayer?.sector || ''}
-              />
-            </div>
-            <div>
-              <label htmlFor="phone" className="block text-sm font-medium text-gray-700">Moble Number:</label>
-              <input
-                id="phone"
-                type="text"
-                name="phone"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.phone || ''}
-              />
-            </div>
-            <div>
-              <label htmlFor="alternatephone" className="block text-sm font-medium text-gray-700">Alternate phone:</label>
-              <input
-                id="alternatephone"
-                type="text"
-                name="alternatephone"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.alternatephone || ''}
-              />
+                defaultValue={singleTpayer?.maritalstatus || ''}
+                >
+                <option value="">Select</option>
+                <option value="married">Married</option>
+                <option value="single">Single</option>
+              </select>
             </div>
             <div>
               <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email:</label>
               <input
                 id="email"
-                type="text"
+                type="email"
                 name="email"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
                 defaultValue={singleTpayer?.email || ''}
+                />
+            </div>
+            <div>
+              <label htmlFor="phonenumber" className="block text-sm font-medium text-gray-700">Moble Number:</label>
+              <input
+                id="phonenumber"
+                type="text"
+                name="phonenumber"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.phonenumber || ''}
+                />
+            </div>
+            <div>
+              <label htmlFor="phonenumber2" className="block text-sm font-medium text-gray-700">Home Number:</label>
+              <input
+                id="phonenumber2"
+                type="text"
+                name="phonenumber2"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.phonenumber2 || ''}
+                />
+            </div>
+            <div>
+              <label htmlFor="bvn" className="block text-sm font-medium text-gray-700">BVN:</label>
+              <input
+                id="bvn"
+                type="text"
+                name="bvn"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.bvn || ''}
+                />
+            </div>
+            <div>
+              <label htmlFor="mothersname" className="block text-sm font-medium text-gray-700">Mother's Surname:</label>
+              <input
+                id="mothersname"
+                type="text"
+                name="mothersname"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.mothersname || ''}
               />
+            </div>
+            {/* <div>
+              <label htmlFor="lga" className="block text-sm font-medium text-gray-700">LGA:</label>
+              <select
+                id="lga"
+                name="lga"
+                value={formData.lga}
+                onChange={(e) => {
+                  handleLGASelection(e);
+                  handleInputChange(e);
+                }}
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 "
+              >
+                <option value="">Select</option>
+                {lgas.map((lga) => (
+                  <option key={lga.id} value={lga.id}>
+                    {lga.name}
+                  </option>
+                ))}
+              </select>
+            </div> */}
+            {/* <div>
+              <label htmlFor="ward" className="block text-sm font-medium text-gray-700">Ward:</label>
+              <select
+                id="ward"
+                name="ward"
+                value={formData.ward}
+                onChange={handleInputChange}
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+              >
+                <option value="">Select Ward</option>
+                {filteredWards.map((ward) => (
+                  <option key={ward.id} value={ward.name}>
+                    {ward.name}
+                  </option>
+                ))}
+              </select>
+            </div> */}
+            <div>
+              <label htmlFor="stateofresidence" className="block text-sm font-medium text-gray-700">State of Residence:</label>
+              <input
+                id="stateofresidence"
+                type="text"
+                name="stateofresidence"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.stateofresidence || ''}
+                
+                />
+            </div>
+            <div>
+              <label htmlFor="stateoforigin" className="block text-sm font-medium text-gray-700">State of Origin:</label>
+              <input
+                id="stateoforigin"
+                type="text"
+                name="stateoforigin"
+                defaultValue={singleTpayer?.stateoforigin || ''}
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                />
+            </div>
+            <div>
+              <label htmlFor="birthplace" className="block text-sm font-medium text-gray-700">Birth Place:</label>
+              <input
+                id="birthplace"
+                type="text"
+                name="birthplace"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.birthplace || ''}
+                />
+            </div>
+
+            <div>
+              <label htmlFor="occupation" className="block text-sm font-medium text-gray-700">Occupation:</label>
+              <input
+                id="occupation"
+                type="text"
+                name="occupation"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.occupation || ''}
+                />
+            </div>
+            <div>
+              <label htmlFor="city" className="block text-sm font-medium text-gray-700">city:</label>
+              <input
+                id="city"
+                type="text"
+                name="city"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.city || ''}
+                />
+            </div>
+
+            <div>
+              <label htmlFor="housestreet" className="block text-sm font-medium text-gray-700">housestreet:</label>
+              <input
+                id="housestreet"
+                type="text"
+                name="housestreet"
+                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
+                defaultValue={singleTpayer?.housestreet || ''}
+                />
             </div>
             <div>
               <label htmlFor="houseno" className="block text-sm font-medium text-gray-700">houseno:</label>
@@ -154,45 +264,13 @@ const UpdateIndividualTp: React.FC<Props> = ({
                 type="text"
                 name="houseno"
                 className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.email || ''}
-              />
-            </div>
-            <div>
-              <label htmlFor="city" className="block text-sm font-medium text-gray-700">City:</label>
-              <input
-                id="city"
-                type="text"
-                name="city"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.city || ''}
-              />
-            </div>
-            <div>
-              <label htmlFor="ward" className="block text-sm font-medium text-gray-700">Ward:</label>
-              <select
-                id="ward"
-                name="ward"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 "
-                defaultValue={singleTpayer?.ward || ''}
-              >
-                <option value="">Select</option>
-              </select>
-            </div>
-            <div>
-              <label htmlFor="street" className="block text-sm font-medium text-gray-700">Street:</label>
-              <input
-                id="street"
-                type="text"
-                name="street"
-                className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-cyan-800 border bg-gray-100 rounded-md px-3 py-2 outline-none"
-                defaultValue={singleTpayer?.street || ''}
+                defaultValue={singleTpayer?.houseno || ''}
               />
             </div>
           </div>
-
           <div className="flex justify-end mt-4">
             <button type="submit" disabled={isSubmitting} className="px-4 py-2 bg-cyan-800 text-white rounded-md shadow-md focus:outline-none hover:bg-cyan-700">
-              {isSubmitting ? 'Submitting...' : 'Update'}
+              {isSubmitting ? 'Submitting...' : 'Submit'}
             </button>
             <button onClick={closeUpdateModal} className="ml-2 px-4 py-2 border border-cyan-800 rounded-md shadow-md focus:outline-none hover:bg-gray-100">Close</button>
           </div>
